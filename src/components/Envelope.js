0b6f1bd366bf1cb6d0a30c9ebed25cf734db7e5c
mohamed.lakkis <mohamed.lakkis@consultores.rnp.br>
2024-09-03 13:30:27 -0300

    feat: new card

--- src/components/Envelope.js
+++ src/components/Envelope.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Envelope.css";
+import ConfirmButton from "./ConfirmButton";
 
 function Envelope() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="envelope">
-      <h2>Convite para a Festa!</h2>
-      <p>Data: 07/09/2024</p>
-      <p>Local: Rua jequinara de goiais, 515</p>
-      <p>Horário: 15h</p>
-      <p>*traga sua bebida </p>
+    <div className="envelope-container">
+      <div className={`envelope ${isOpen ? "open" : ""}`}>
+        <div onClick={handleClick} className="envelope-flap">
+          <h1>Convite Especial</h1>
+          <span>clique aqui.</span>
+        </div>
+        <div className="envelope-content">
+          <h4>Você está Convidado!</h4>
+          <p>Data: 07/09/2024</p>
+          <p>Local: Rua Jequirana de goiais, 515</p>
+          <p>Horário: 15h</p>
+          <p>*traga sua bebida</p>
+
+          <p>Por favor, confirme sua presença abaixo.</p>
+          <ConfirmButton />
+        </div>
+      </div>
     </div>
   );
 }
